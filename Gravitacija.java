0def56284e457d5f037736fa0a2c69b81124b1c2
tadejparis <tadej.paris@protonmail.com>
2023-02-26 19:34:39 +0100

    resolve merge conflict

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,7 +5,6 @@ public class Gravitacija {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-<<<<<<< HEAD
         double C = Double.parseDouble("6.674E-11");
         double M = Double.parseDouble("5.972E24");
         double r = Double.parseDouble("6.371E6");
@@ -14,15 +13,10 @@ public class Gravitacija {
         double a = (C*M)/((r+v)*(r+v));
 
 		izpis(v, a);
->>>>>>> origin/ime-veje
-	}
-}
-=======
-public class Gravitacija {
+
 	public static void izpis(int visina, double gravitacija) {
 		System.out.println(visina);
 		System.out.println(gravitacija);
 		
 	}
 }
->>>>>>> origin/izpis

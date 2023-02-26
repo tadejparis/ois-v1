f9acea26c44b11b0e3a75ba238e1c823ce600650
LukaDegan <105129163+LukaDegan@users.noreply.github.com>
2023-02-26 15:49:01 +0100

    Add files via upload

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,12 @@
 public class Gravitacija {
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
+		izpis(visina, gravitacija);
+	}
+	
+	public static void izpis(int visina, double gravitacija) {
+		System.out.println(visina);
+		System.out.println(gravitacija);
+		
 	}
 }
